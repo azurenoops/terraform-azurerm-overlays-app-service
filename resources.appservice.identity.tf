236555b49eaa026fea9e45fe9a8829d2f9d75599
John Spinella <jrspinella@users.noreply.github.com>
2023-08-11 08:07:14 -0400

    Delete data lookup

--- resources.appservice.identity.tf
+++ resources.appservice.identity.tf
@@ -6,11 +6,3 @@ resource "azurerm_user_assigned_identity" "app_identity" {
   name                = local.app_user_assigned_identity_name
   resource_group_name = local.resource_group_name
 }
-
-data "azurerm_user_assigned_identity" "app_identity" {
-  depends_on = [
-    azurerm_user_assigned_identity.app_identity
-  ]
-  name = local.app_user_assigned_identity_name
-  resource_group_name = local.resource_group_name
-}
